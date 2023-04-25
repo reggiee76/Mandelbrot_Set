bf14fe5f25ad1b41dbf11194f2d00647f1d64c75
reggiee76 <90946376+reggiee76@users.noreply.github.com>
2023-04-24 20:10:32 -0700

    update main

--- main.cpp
+++ main.cpp
@@ -1,11 +1,42 @@
 #include <iostream>
 #include <complex>
 #include "ComplexPlane.h"
+#include <SFML/Graphics.hpp>
+using namespace sf;
 using namespace std;
-
 
 int main()
 {
+  // Get the desktop resolution
+	const unsigned int width = VideoMode::getDesktopMode().width;
+	const unsigned int height = VideoMode::getDesktopMode().height;
+
+
+	// Calculate the aspect ratio of the monitor
+	float aspectRatio = static_cast<float>(height) / static_cast<float>(width);
+
+	// Construct the window
+	RenderWindow window(VideoMode(width, height), "Mandelbrot Set");
+
+
+	// Construct an object of type ComplexPlane
+	ComplexPlane plane(16.0f / 9.0f);
+
+	// Create a Font Object
+	Font font;
+	if (!font.loadFromFile("Orbitron.ttf"))
+	{
+		cout << "Font failed to load" << endl;
+	}
+
+	// Create a Text Object
+	Text text;
+	text.setFont(font);
+	text.setCharacterSize(24);
+	text.setFillColor(Color::White);
+	text.setPosition(10, 10);
+	
+	// Construct Vertex Array
 
 
 
